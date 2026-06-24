c7748b06693cab6712e262f5b875d5773b5e6a89
Al Abid <alabid172s@gmail.com>
2026-06-24 17:13:05 +0600

    Version 1

---
+++ Lovely Birthday Wish/setup.js
@@ -0,0 +1,26 @@
+export const myGF = {
+    // Her Name here
+    name: 'Her Name',
+
+    firstMeetDate: '2000-01-01',    // the day u meet first
+    firstDate: '2000-01-01',    // the day u went on a day first time
+    favMemoryDate: '2000-01-01',    // the day of your favorite memory
+
+    //favorite memory and first date and first time meeting are consider separete things
+
+
+    // Her Name here
+    birthDayWish: "write a peragraph wishing her in 200-244 cherecter",
+    firstMeetExperiance: 'Replace this with how it all started',
+    firstDateExperiance: "Swap in the memory you both still laugh about",
+    favMemory: "Tell about the favorite memory",
+    stillInLoveText: "Wrap it up with where you are now and why this year is going to be even better.",
+    
+    
+    whyILove: {
+        first: 'Replace this with why u love her',  //most importent reason
+        second: 'Replace this with why u love her', //Second most importent reason
+        third: 'Replace this with why u love her',  //Thirsd most importent reason
+        fourth: 'Replace this with why u love her'  //Fourth most importent reason
+    }
+}
